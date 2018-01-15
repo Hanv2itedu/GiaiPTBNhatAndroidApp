bfe51b41e9d66c3f76d790efbf5aa7f7304bcb39
Hanv2itedu <hanv2.itedu@gmail.com>
2018-01-15 16:50:47 +0700

    Nguyen Van Ha - Add activity main and GPTBN activity

--- app/build/generated/source/r/debug/android/support/compat/R.java
+++ app/build/generated/source/r/debug/android/support/compat/R.java
@@ -22,10 +22,10 @@ public final class R {
         public static final int abc_action_bar_embed_tabs = 0x7f040000;
     }
     public static final class color {
-        public static final int notification_action_color_filter = 0x7f05004b;
-        public static final int notification_icon_bg_color = 0x7f05004c;
-        public static final int ripple_material_light = 0x7f050058;
-        public static final int secondary_text_default_material_light = 0x7f05005a;
+        public static final int notification_action_color_filter = 0x7f05004c;
+        public static final int notification_icon_bg_color = 0x7f05004d;
+        public static final int ripple_material_light = 0x7f050059;
+        public static final int secondary_text_default_material_light = 0x7f05005b;
     }
     public static final class dimen {
         public static final int compat_button_inset_horizontal_material = 0x7f06004a;
@@ -71,35 +71,35 @@ public final class R {
         public static final int actions = 0x7f080018;
         public static final int async = 0x7f08001e;
         public static final int blocking = 0x7f080022;
-        public static final int chronometer = 0x7f08002d;
-        public static final int forever = 0x7f08004e;
-        public static final int icon = 0x7f080052;
-        public static final int icon_group = 0x7f080053;
-        public static final int info = 0x7f080056;
-        public static final int italic = 0x7f080057;
-        public static final int line1 = 0x7f08005b;
-        public static final int line3 = 0x7f08005c;
-        public static final int normal = 0x7f080068;
-        public static final int notification_background = 0x7f080069;
-        public static final int notification_main_column = 0x7f08006a;
-        public static final int notification_main_column_container = 0x7f08006b;
-        public static final int right_icon = 0x7f080076;
-        public static final int right_side = 0x7f080077;
-        public static final int text = 0x7f0800a0;
-        public static final int text2 = 0x7f0800a1;
-        public static final int time = 0x7f0800a7;
-        public static final int title = 0x7f0800a8;
+        public static final int chronometer = 0x7f08002f;
+        public static final int forever = 0x7f080050;
+        public static final int icon = 0x7f080054;
+        public static final int icon_group = 0x7f080055;
+        public static final int info = 0x7f080058;
+        public static final int italic = 0x7f080059;
+        public static final int line1 = 0x7f08005d;
+        public static final int line3 = 0x7f08005e;
+        public static final int normal = 0x7f08006a;
+        public static final int notification_background = 0x7f08006b;
+        public static final int notification_main_column = 0x7f08006c;
+        public static final int notification_main_column_container = 0x7f08006d;
+        public static final int right_icon = 0x7f080078;
+        public static final int right_side = 0x7f080079;
+        public static final int text = 0x7f0800a2;
+        public static final int text2 = 0x7f0800a3;
+        public static final int time = 0x7f0800a9;
+        public static final int title = 0x7f0800aa;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f090009;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a002a;
-        public static final int notification_action_tombstone = 0x7f0a002b;
-        public static final int notification_template_custom_big = 0x7f0a0032;
-        public static final int notification_template_icon_group = 0x7f0a0033;
-        public static final int notification_template_part_chronometer = 0x7f0a0037;
-        public static final int notification_template_part_time = 0x7f0a0038;
+        public static final int notification_action = 0x7f0a002b;
+        public static final int notification_action_tombstone = 0x7f0a002c;
+        public static final int notification_template_custom_big = 0x7f0a0033;
+        public static final int notification_template_icon_group = 0x7f0a0034;
+        public static final int notification_template_part_chronometer = 0x7f0a0038;
+        public static final int notification_template_part_time = 0x7f0a0039;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0c002a;
--- app/src/main/java/com/example/hanguyen/demo/MainActivity.java
+++ app/src/main/java/com/example/hanguyen/demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hanguyen.demo;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.input.InputManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -17,100 +19,38 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener{
-    int soA, soB, result;
-    EditText editSoA, editSoB;
-    TextView txtResult;
-    Button btnResult, btnReset;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnPTB1, btnPTB2;    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //gan cac element xml thanh cac bien java
-        editSoA = (EditText) findViewById(R.id.editSoA);
-        editSoB = (EditText) findViewById(R.id.editSoB);
-        txtResult = (TextView) findViewById(R.id.txtResult);
-        btnResult = (Button) findViewById(R.id.btnResult);
-        btnReset = (Button) findViewById(R.id.btnReset);
-        //bat su kien onKey cho editor text
-        // muc dich: khong cho xuong hang khi bam phim enter
-        editSoA.setOnKeyListener(this);
-        editSoB.setOnKeyListener(this);
-        //bat su kien onclick cho buttons
-        btnResult.setOnClickListener(this);
-        btnReset.setOnClickListener(this);
 
+        Intent intent = getIntent();
+        if(intent != null){
+            Toast.makeText(MainActivity.this,
+                    "Last item you did is " + intent.getStringExtra("soA") + "x + " +
+                    intent.getStringExtra("soB") + "= 0" + intent.getStringExtra("result"),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        btnPTB1 = (Button) findViewById(R.id.btnPTB1);
+        btnPTB2 = (Button) findViewById(R.id.btnPTB2);
+
+        btnPTB1.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        hideTheKeyBoard();//an key board sau khi click
-        String sSoA = editSoA.getText().toString();// get value tu editor
-        String sSoB = editSoB.getText().toString();
+    public void onClick(View v) {       
         switch (v.getId()){
-            case R.id.btnResult : //neu nhu click vao nut btnResult
-                showResult(sSoA,sSoB);// thuc hien ham show
+            case R.id.btnPTB1 : //neu nhu click vao nut btnPTB1
+                Intent newIntent = new Intent(MainActivity.this, ActivityPhuongTrinhBacNhat.class);
+                startActivity(newIntent);
                 break;
-            case R.id.btnReset : //neu nhu click vao nut btnReset
-                resetAll(); // thuc hien ham resetAll,
+            case R.id.btnPTB2 : //neu nhu click vao nut btnPTB2
                 break;
         }
     }
-    public  void showResult(String sSoA, String sSoB){
-        //kiem tra xem la da nhap du truong hay chua
-        if(sSoA.isEmpty() || sSoB.isEmpty() ){
-            Toast.makeText(MainActivity.this, "Bạn nhập thiếu ít nhất một trường! Mời nhập lại", Toast.LENGTH_LONG).show();
-            return;
-        }
-        //bat ngoai le khi nguoi dung nhap vao chu cai hay khoang trang
-        try{
-            soA = Integer.parseInt(sSoA);
-            soB = Integer.parseInt(sSoB);
-        }catch (Exception e){
-            Toast.makeText(MainActivity.this, "Bạn Nhap Sai! Yêu Cầu Nhập Lại", Toast.LENGTH_LONG).show();
-            resetEditor();
-            return;
-        }
-        //bat ngoai le khi giai phuong trinh
-        try{
-            txtResult.setText("Kết quả:  " + String.valueOf(giaiPTB1(soA,soB)));
-        }catch (PTBNhatException ptEx){
-            txtResult.setText("Kết quả:  " + ptEx.getMessage());
-            resetEditor();
-        }
-    }
 
-    public  void resetEditor(){
-        editSoA.setText("");
-        editSoB.setText("");
-    }
-
-    public void resetAll(){
-        resetEditor();
-        txtResult.setText("Kết quả:  ");
-    }
-
-    public void hideTheKeyBoard(){
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
-    }
-    // ham giai phuong trinh co nem ngoai le
-    public float giaiPTB1(int a, int b) throws PTBNhatException{
-        if(a==0){
-            throw new PTBNhatException("a phai khac 0"); // nem ngoai le thong qua class PTBexception bang mot messeage
-        }else{
-            return (float) -b/a; // return kq
-        }
-    }
-
-    @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
-        //neu nguoi dung bam phim enter thi khong lam gi
-        if(keyCode == KeyEvent.KEYCODE_ENTER){
-            return true;
-        }
-        //mac dinh bat tat ca cac key khac voi enter
-        return super.onKeyDown(keyCode, event);
-    }
 }
